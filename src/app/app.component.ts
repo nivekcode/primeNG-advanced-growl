5fe8832f65ee098dc73fc09806b5d2371e1baa1a
kreuzerk <kevin.kreuzer90@icloud.com>
2017-07-08 22:08:47 +0200

    Included styles and added buttons to test the messageservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,23 @@ export class AppComponent {
     constructor(private advMessagesService: AdvMessagesService) {
     }
 
-    public createInfoMessage() {
-        this.advMessagesService.createInfoMessage('Awesome message content', 'Awesome summary');
+    public createInfoMessage(): void {
+        this.advMessagesService.createInfoMessage('Awesome success message content', 'Awesome info');
+    }
+
+    public createSuccessMessage(): void {
+        this.advMessagesService.createSuccessMessage('Awesome success message content', 'Awesome success');
+    }
+
+    public createWarningMessage(): void {
+        this.advMessagesService.createWarningMessage('Important warning content', 'Important warning');
+    }
+
+    public createErrorMessage(): void {
+        this.advMessagesService.createErrorMessage('Awful error message', 'Error summary');
+    }
+
+    public clearMessages(): void {
+        this.advMessagesService.clearMessages();
     }
 }
