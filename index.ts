d9888d38d519750e8c438a21581623a890ceb819
kreuzerk <kevin.kreuzer90@icloud.com>
2017-11-13 16:06:12 +0100

    Rename test to src and fix unit test execution

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /**
  * Created by kevinkreuzer on 08.07.17.
  */
-export * from './test/app/lib/adv-growl.module'
-export * from './test/app/lib/adv-growl.service'
+export * from './src/app/lib/adv-growl.module'
+export * from './src/app/lib/adv-growl.service'
