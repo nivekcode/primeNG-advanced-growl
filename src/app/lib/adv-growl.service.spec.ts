1213a5837fc185614527baf36dafc584b49d0bba
kreuzerk <kevin.kreuzer90@icloud.com>
2017-12-25 00:16:06 +0100

    Extend unit testsπ

--- src/app/lib/adv-growl.service.spec.ts
+++ src/app/lib/adv-growl.service.spec.ts
@@ -11,7 +11,7 @@ describe('Message Service', () => {
         });
     });
 
-    describe('Creating simple messages', () => {
+    describe('createSuccessMessage', () => {
 
         it('should create a successmessage and stream it into the message subject', done => {
             // given
@@ -37,6 +37,34 @@ describe('Message Service', () => {
             sut.createSuccessMessage(messageContent, messageSummary);
         });
 
+        it('should create a successmessage with additional properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService);
+            const messageContent = 'Awesome Message';
+            const messageSummary = 'Success';
+            const additionalProperties = {title: 'test', description: 'Test message'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'success',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime: undefined,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage)
+                    done();
+                });
+            // when
+            sut.createSuccessMessage(messageContent, messageSummary, additionalProperties);
+        })
+    })
+
+    describe('createErrorMessage', () => {
+
         it('should create a errormessage and stream it into the message subject', done => {
             // given
             const sut = TestBed.get(AdvGrowlService);
@@ -60,6 +88,34 @@ describe('Message Service', () => {
             // when
             sut.createErrorMessage(messageContent, messageSummary);
         });
+
+        it('should create a errormessage with additional properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService);
+            const messageContent = 'Awful Error';
+            const messageSummary = 'Error';
+            const additionalProperties = {title: 'Error message', description: 'something went wrong'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'error',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime: undefined,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createErrorMessage(messageContent, messageSummary, additionalProperties);
+        })
+    })
+
+    describe('createInfoMessage', () => {
 
         it('should create a infomessage and stream it into the message subject', done => {
             // given
@@ -85,6 +141,34 @@ describe('Message Service', () => {
             sut.createInfoMessage(messageContent, messageSummary);
         });
 
+        it('should create a infomessage with additonal properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService);
+            const messageContent = 'Super important information';
+            const messageSummary = 'Information';
+            const additionalProperties = {title: 'Info message', description: 'Just some information'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'info',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime: undefined,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createInfoMessage(messageContent, messageSummary, additionalProperties);
+        })
+    })
+
+    describe('createWarningMessage', () => {
+
         it('should create a warningmessage and stream it into the message subject', done => {
             // given
             const sut = TestBed.get(AdvGrowlService);
@@ -108,99 +192,13 @@ describe('Message Service', () => {
             // when
             sut.createWarningMessage(messageContent, messageSummary);
         });
-    })
 
-    describe('Streaming messages with additional properties', () => {
-
-        it('should create a successmessage and stream it into the message subject', done => {
-            // given
-            const sut = TestBed.get(AdvGrowlService);
-            const messageContent = 'Awesome Message';
-            const messageSummary = 'Success';
-            const additionalProperties = {
-                clickMessage: 'Awesome click'
-            };
-            const id = 1;
-            spyOn(sut, 'getTimeStamp').and.returnValue(id);
-            const expectedMessage = {
-                id,
-                severity: 'success',
-                summary: messageSummary,
-                detail: messageContent,
-                lifeTime: undefined,
-                additionalProperties
-            };
-            sut.getMessageStream()
-                .subscribe(message => {
-                    expect(message).toEqual(expectedMessage);
-                    done();
-                });
-            // when
-            sut.createSuccessMessage(messageContent, messageSummary, additionalProperties);
-        });
-
-        it('should create a errormessage and stream it into the message subject', done => {
-            // given
-            const sut = TestBed.get(AdvGrowlService);
-            const messageContent = 'Awful Error';
-            const messageSummary = 'Error';
-            const additionalProperties = {
-                clickMessage: 'Awesome click'
-            };
-            const id = 1;
-            spyOn(sut, 'getTimeStamp').and.returnValue(id);
-            const expectedMessage = {
-                id,
-                severity: 'error',
-                summary: messageSummary,
-                detail: messageContent,
-                lifeTime: undefined,
-                additionalProperties
-            };
-            sut.getMessageStream()
-                .subscribe(message => {
-                    expect(message).toEqual(expectedMessage);
-                    done();
-                });
-            // when
-            sut.createErrorMessage(messageContent, messageSummary, additionalProperties);
-        });
-
-        it('should create a infomessage and stream it into the message subject', done => {
-            // given
-            const sut = TestBed.get(AdvGrowlService);
-            const messageContent = 'Super important information';
-            const messageSummary = 'Information';
-            const additionalProperties = {
-                clickMessage: 'Awesome click'
-            };
-            const id = 1;
-            spyOn(sut, 'getTimeStamp').and.returnValue(id);
-            const expectedMessage = {
-                id,
-                severity: 'info',
-                summary: messageSummary,
-                detail: messageContent,
-                lifeTime: undefined,
-                additionalProperties
-            };
-            sut.getMessageStream()
-                .subscribe(message => {
-                    expect(message).toEqual(expectedMessage);
-                    done();
-                });
-            // when
-            sut.createInfoMessage(messageContent, messageSummary, additionalProperties);
-        });
-
-        it('should create a warningmessage and stream it into the message subject', done => {
+        it('should create a warningmessage with additional properties and stream it into the message subject', done => {
             // given
             const sut = TestBed.get(AdvGrowlService);
             const messageContent = 'Super important warning';
             const messageSummary = 'Warning';
-            const additionalProperties = {
-                clickMessage: 'Awesome click'
-            };
+            const additionalProperties = {title: 'Warning message', description: 'Some warnings'}
             const id = 1;
             spyOn(sut, 'getTimeStamp').and.returnValue(id);
             const expectedMessage = {
@@ -221,7 +219,7 @@ describe('Message Service', () => {
         });
     })
 
-    describe('Clearing messages', () => {
+    describe('clearMessages', () => {
         it('should stream a new message in the clearStream when calling clearMessages', done => {
             // given
             const sut = TestBed.get(AdvGrowlService);
@@ -231,4 +229,220 @@ describe('Message Service', () => {
             sut.clearMessages();
         });
     })
-});
+
+    describe('createTimedSuccessMessage', () => {
+
+        it('should create a timed successmessage and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService);
+            const messageContent = 'Awesome Message';
+            const messageSummary = 'Success';
+            const lifeTime = 2000;
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'success',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties: undefined
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage)
+                    done();
+                });
+            // when
+            sut.createTimedSuccessMessage(messageContent, messageSummary, lifeTime);
+        });
+
+        it('should create a timed successmessage with additional properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Awesome Message'
+            const messageSummary = 'Success'
+            const lifeTime = 2000
+            const additionalProperties = {title: 'test', description: 'Test message'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'success',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage)
+                    done();
+                });
+            // when
+            sut.createTimedSuccessMessage(messageContent, messageSummary, lifeTime, additionalProperties);
+        })
+    })
+
+    describe('createTimedErrorMessage', () => {
+
+        it('should create a timed errormessage and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService);
+            const messageContent = 'Awful Error';
+            const messageSummary = 'Error';
+            const lifeTime = 2000;
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'error',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties: undefined
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createTimedErrorMessage(messageContent, messageSummary, lifeTime);
+        });
+
+        it('should create a timed errormessage with additional properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Awful Error'
+            const messageSummary = 'Error'
+            const lifeTime = 2000
+            const additionalProperties = {title: 'Error message', description: 'something went wrong'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'error',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createTimedErrorMessage(messageContent, messageSummary, lifeTime, additionalProperties);
+        })
+    })
+
+    describe('createTimedInfoMessage', () => {
+
+        it('should create a timed infomessage and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Super important information'
+            const messageSummary = 'Information'
+            const lifeTime = 2000
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'info',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties: undefined
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createTimedInfoMessage(messageContent, messageSummary, lifeTime);
+        });
+
+        it('should create a timed infomessage with additonal properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Super important information'
+            const messageSummary = 'Information'
+            const lifeTime = 2000
+            const additionalProperties = {title: 'Info message', description: 'Just some information'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'info',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createTimedInfoMessage(messageContent, messageSummary, lifeTime, additionalProperties);
+        })
+    })
+
+    describe('createTimedWarningMessage', () => {
+
+        it('should create a timed warningmessage and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Super important warning'
+            const messageSummary = 'Warning'
+            const lifeTime = 2000
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'warn',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties: undefined
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                });
+            // when
+            sut.createTimedWarningMessage(messageContent, messageSummary, lifeTime);
+        });
+
+        it('should create a timed warningmessage with additional properties and stream it into the message subject', done => {
+            // given
+            const sut = TestBed.get(AdvGrowlService)
+            const messageContent = 'Super important warning'
+            const messageSummary = 'Warning'
+            const lifeTime = 2000
+            const additionalProperties = {title: 'Warning message', description: 'Some warnings'}
+            const id = 1;
+            spyOn(sut, 'getTimeStamp').and.returnValue(id);
+            const expectedMessage = {
+                id,
+                severity: 'warn',
+                summary: messageSummary,
+                detail: messageContent,
+                lifeTime,
+                additionalProperties
+            };
+            sut.getMessageStream()
+                .subscribe(message => {
+                    expect(message).toEqual(expectedMessage);
+                    done();
+                })
+            // when
+            sut.createTimedWarningMessage(messageContent, messageSummary, lifeTime, additionalProperties);
+        })
+    })
+})
